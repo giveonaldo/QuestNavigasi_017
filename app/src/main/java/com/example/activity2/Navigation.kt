2b8874c9d69386e41cbb107a3bd02654980f3d06
Uknown <abriansyahadam5@gmail.com>
2025-08-12 16:23:45 +0700

    Enumerasi class Navigasi

--- app/src/main/java/com/example/activity2/Navigation.kt
+++ app/src/main/java/com/example/activity2/Navigation.kt
@@ -1,2 +1,18 @@
 package com.example.activity2
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activity2.pages.Formulir
+import com.example.activity2.pages.ShowData
+
+enum class Navigasi {
+    Formulir,
+    Data
+}
