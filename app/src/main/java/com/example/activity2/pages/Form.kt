876464d640168726f61fa11eb4452d6038fcb006
Uknown <abriansyahadam5@gmail.com>
2025-08-12 16:12:48 +0700

    inisilisasi variable

--- app/src/main/java/com/example/activity2/pages/Form.kt
+++ app/src/main/java/com/example/activity2/pages/Form.kt
@@ -3,6 +3,9 @@ package com.example.activity2.pages
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Formulir() {
-    
+fun Formulir(onSubmitBtnClick: () -> Unit) {
+    var txtNama by remember { mutableStateOf("") }
+    var txtEmail by remember { mutableStateOf("") }
+    val option = listOf("Laki-laki", "Perempuan")
+    var pilihan by remember { mutableStateOf(option[0]) }
 }
