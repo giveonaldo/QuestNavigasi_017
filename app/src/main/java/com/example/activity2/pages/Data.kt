fd59146c487cbb870d86ab0385e308fd90f94067
Uknown <abriansyahadam5@gmail.com>
2025-08-12 16:20:27 +0700

    define function ShowData

--- app/src/main/java/com/example/activity2/pages/Data.kt
+++ app/src/main/java/com/example/activity2/pages/Data.kt
@@ -1,4 +1,24 @@
 package com.example.activity2.pages
 
-class Data {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShowData(
+    onBackBtnClick: () -> Unit
+) {
+
 }
