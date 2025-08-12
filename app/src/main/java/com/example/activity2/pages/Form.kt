14ec81fe4e939fe90837a4696f9f125318e9a277
Uknown <abriansyahadam5@gmail.com>
2025-08-12 16:14:17 +0700

    define function not empty form field

--- app/src/main/java/com/example/activity2/pages/Form.kt
+++ app/src/main/java/com/example/activity2/pages/Form.kt
@@ -1,6 +1,8 @@
 package com.example.activity2.pages
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun Formulir(onSubmitBtnClick: () -> Unit) {
@@ -8,4 +10,13 @@ fun Formulir(onSubmitBtnClick: () -> Unit) {
     var txtEmail by remember { mutableStateOf("") }
     val option = listOf("Laki-laki", "Perempuan")
     var pilihan by remember { mutableStateOf(option[0]) }
+
+    fun enabled(): Boolean {
+        if (txtNama.isNotEmpty() && txtEmail.isNotEmpty()) {
+            return true
+        }
+        return false
+    }
+
+
 }
